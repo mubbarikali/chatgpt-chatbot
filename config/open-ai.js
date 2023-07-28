c58a3257b044ae7c8912074821df41780e250798
Mubbarik Ali <mubbarikali@gmail.com>
2023-07-28 22:25:15 +0500

    Working model.

--- config/open-ai.js
+++ config/open-ai.js
@@ -1,10 +1,10 @@
-import { Configuration, OpenAIApi } from "openai";
+import { Configuration, OpenAIApi } from 'openai';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const configuration = new Configuration ({
-    apiKey: process.env.OPENAI_API_KEY
-})
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 const openai = new OpenAIApi(configuration);
 
